051ad9cc7adc4cb0a95ed4ed491a95198f1ef2fe
MiloticHwang <ghkdeh73@comixv.onmicrosoft.com>
2020-02-05 15:15:00 +0900

    added output "Tell Your World"

--- hello.py
+++ hello.py
@@ -2,8 +2,4 @@
 // First: GitHub modification
 // Second: Local repository modification
 print("Hello world")
-print("Hello world")
-print("Tell Your World")
-print("Tell his world")
-print("Tell her world")
-print("Tell my world")
+print("Tell Your world")
